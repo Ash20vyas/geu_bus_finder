8ea09b2006f573195d2432ec3558fefe67f27998
affanthegreat <32616893+affanthegreat@users.noreply.github.com>
2021-10-30 22:52:27 +0530

    fixed picture

--- geu_bus_driver/lib/designs.dart
+++ geu_bus_driver/lib/designs.dart
@@ -8,12 +8,18 @@ double h4 = 17;
 double h5 = 15;
 double h6 = 13;
 
+var logoRed  = Color(0xffDFA3A3);
+var black = Color(0xff343434);
+var textBoxColor  = Color(0xffECEAEA);
+var boxRed = Color(0xffF5C5C5);
+var boxRedText = Color(0xffD68585);
+var subTitleColor = Color(0xffDACCCC);
 
 
 TextStyle poppins(Color col, [double? x, FontWeight? weight]) {
-  return GoogleFonts.poppins(textStyle: TextStyle(height: 1.5, letterSpacing: -0.5, fontWeight: weight ?? FontWeight.bold, color: col, fontSize: x ?? h5));
+  return GoogleFonts.poppins(textStyle: TextStyle(height: 1, letterSpacing: -0.5, fontWeight: weight ?? FontWeight.bold, color: col, fontSize: x ?? h5));
 }
 
 TextStyle montserrat(Color col, [double? x, FontWeight? weight]) {
-  return GoogleFonts.montserrat(textStyle: TextStyle(height: 1.5, fontWeight: weight ?? FontWeight.bold, color: col, fontSize: x ?? h5));
+  return GoogleFonts.montserrat(textStyle: TextStyle(height: 1, fontWeight: weight ?? FontWeight.bold, color: col, fontSize: x ?? h5));
 }
--- geu_bus_driver/lib/loginpage.dart
+++ geu_bus_driver/lib/loginpage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geu_bus_driver/designs.dart';
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
@@ -17,7 +18,68 @@ class _LoginPageState extends State<LoginPage> {
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Logo(),
+          Container(
+            alignment: Alignment.centerLeft,
+            margin: EdgeInsets.only(left: 15,right: 15),
+            child: Text(
+              "Phone number",
+              style: montserrat(black.withOpacity(0.6),h3),
+            ),
+          ),
+          Container(
+            height: 40,
+            width: double.infinity,
+            margin: EdgeInsets.only(left: 15,right: 15),
+            decoration: BoxDecoration(
+              color: textBoxColor,
+              borderRadius:BorderRadius.circular(5),
+              border: Border.all(
+                color: Colors.grey.shade400
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.shade300,
+                  offset: Offset(
+                    5.0,
+                    5.0,
+                  ),
+                  blurRadius: 10.0,
+                  spreadRadius: 2.0,
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
 }
+
+class Logo extends StatelessWidget {
+  const Logo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        alignment: Alignment.centerLeft,
+        padding: EdgeInsets.all(15),
+        child: RichText(
+          text: TextSpan(
+              text: 'GEU\n',
+              style: montserrat(black,h2,FontWeight.normal),
+              children: <TextSpan>[
+                TextSpan(text: 'BUS\n',
+                  style: montserrat(logoRed,h1+30,FontWeight.bold),
+                ),
+                TextSpan(text: 'FINDER\n',
+                  style: montserrat(black,h1+30,FontWeight.bold),
+                )
+              ]
+          ),
+        )
+    );
+  }
+}
+
