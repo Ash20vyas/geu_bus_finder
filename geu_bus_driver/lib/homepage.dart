1f83cf5bbe1ea512a209f6638c4dd4893137d54f
Ashutosh Vyas <54895033+Ash20vyas@users.noreply.github.com>
2021-10-31 02:06:35 +0530

    homepage completed

--- geu_bus_driver/lib/homepage.dart
+++ geu_bus_driver/lib/homepage.dart
@@ -1,4 +1,8 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:geu_bus_driver/designs.dart';
+
+bool isStarted = false;
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,59 +13,183 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   var busNo = 15;
-  var driverName = "Mia Khalifa";
-  var phoneNumber = "6969696969";
-  var boxColor = Colors.green[300];
-  var altBoxColor = Colors.red[300];
-  var boxWord = "START";
-  var altBoxWord = "END";
+  var driverName = "Salman\nKhan";
+  var phoneNumber = "1234567890";
+  bool isChecked = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Stack(
       children: [
-        Container(
+        AnimatedContainer(
+          duration: Duration(microseconds: 350),
           decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("assets/graphic_era.jpg"),
-              fit: BoxFit.cover,
-            ),
-          ),
-        ),
-        Container(
-          decoration: BoxDecoration(color: Colors.black38),
+              gradient: LinearGradient(
+            begin: Alignment.bottomRight,
+            end: Alignment.topLeft,
+            colors: [
+              isStarted ? Color(0xffF08A8A) : Color(0xffC0F9C2),
+              isStarted ? Color(0xffF08A8A) : Color(0xffC0F9C2),
+              isStarted
+                  ? Color(0xffF08A8A).withOpacity(0.2)
+                  : Color(0xffC0F9C2).withOpacity(0.2),
+            ],
+          )),
         ),
         Column(
           children: [
-            Text("Bus No"),
-            Text(busNo.toString()),
-            Text("Name"),
-            Text(driverName),
-            Text("Phone Number"),
-            Text(phoneNumber),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.2,
+            ),
+            SubHeading(value: "Bus No"),
+            Heading(value: busNo.toString()),
+            SubHeading(value: "Name"),
+            Heading(value: driverName),
+            SubHeading(value: "Phone Number"),
+            NumHeading(value: phoneNumber),
           ],
         ),
-        Container(
-            padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
-            height: 30,
-            width: double.infinity,
-            decoration: BoxDecoration(color: boxColor),
-            alignment: Alignment.bottomCenter,
-            child: TextButton(
-              child: Text(boxWord),
-              onPressed: () {
-                setState(() {
-                  var x = boxColor;
-                  boxColor = altBoxColor;
-                  altBoxColor = x;
-                  var y = boxWord;
-                  boxWord = altBoxWord;
-                  altBoxWord = y;
-                });
-              },
-            )),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Row(children: [
+              Container(
+                margin: EdgeInsets.only(left: 5),
+                child: Checkbox(
+                  value: this.isChecked,
+                  onChanged: (bool? value) {
+                    if (!isStarted) {
+                      setState(() {
+                        this.isChecked = value!;
+                      });
+                    }
+                  },
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(15),
+                child: Text(
+                  "I acknowledge that I’m ready to\nstart the bus.",
+                  style: poppins(black, h4, FontWeight.normal),
+                ),
+              )
+            ]),
+            AnimatedContainer(
+              duration: Duration(milliseconds: 350),
+              height: MediaQuery.of(context).size.height * 0.15,
+              width: double.infinity,
+              margin: EdgeInsets.fromLTRB(
+                  15,
+                  MediaQuery.of(context).size.height * 0.02,
+                  15,
+                  MediaQuery.of(context).size.height * 0.07),
+              decoration: BoxDecoration(
+                color: isStarted ? Color(0xffFA9587) : Color(0xff9CE89F),
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.shade600,
+                    offset: const Offset(
+                      5.0,
+                      5.0,
+                    ),
+                    blurRadius: 10.0,
+                    spreadRadius: 2.0,
+                  ),
+                ],
+              ),
+              child: TextButton(
+                child: Text(isStarted ? "END" : "START",
+                    style: montserrat(
+                        isStarted ? Color(0xffED4646) : Color(0xff5EA16C),
+                        h1 + 15,
+                        FontWeight.bold)),
+                onPressed: () {
+                  if (isStarted) {
+                    setState(() {
+                      isChecked = false;
+                      isStarted = !isStarted;
+                    });
+                  } else {
+                    if (isChecked) {
+                      setState(() {
+                        isStarted = !isStarted;
+                      });
+                    } else {
+                      final snackBar = SnackBar(
+                        content: Text(
+                          'Please Acknowledge',
+                          style:
+                              montserrat(Colors.white, h3, FontWeight.normal),
+                        ),
+                        backgroundColor: Colors.red,
+                        duration: Duration(milliseconds: 500),
+                      );
+                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                    }
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
       ],
     ));
+  }
+}
+
+class SubHeading extends StatelessWidget {
+  String value;
+  SubHeading({required this.value, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 350),
+      alignment: Alignment.centerLeft,
+      margin: EdgeInsets.only(left: 15, top: 20, bottom: 5),
+      child: Text(
+        value,
+        style: montserrat(Colors.grey.shade700, h4),
+      ),
+    );
+  }
+}
+
+class Heading extends StatelessWidget {
+  String value;
+  Heading({required this.value, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 350),
+      alignment: Alignment.centerLeft,
+      margin: EdgeInsets.only(left: 15),
+      child: AutoSizeText(
+        value,
+        maxLines: 2,
+        style: montserrat(isStarted ? Colors.white : black, h1 + 15),
+      ),
+    );
+  }
+}
+
+class NumHeading extends StatelessWidget {
+  String value;
+  NumHeading({required this.value, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 350),
+      alignment: Alignment.centerLeft,
+      margin: EdgeInsets.only(left: 15),
+      child: Text(
+        value,
+        style: montserrat(isStarted ? Colors.white : black, h1 + 7),
+      ),
+    );
   }
 }
--- geu_bus_driver/lib/loginpage.dart
+++ geu_bus_driver/lib/loginpage.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:geu_bus_driver/designs.dart';
+
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
@@ -10,35 +11,39 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   String? phoneNumber;
-  void phoneNumberLogin() async{
-    if(phoneNumber!.length != 10){
-      final snackBar = SnackBar(content: Text('Invalid phone Number'));
+  void phoneNumberLogin() async {
+    if (phoneNumber!.length != 10) {
+      final snackBar = SnackBar(
+        content: Text(
+          'Invalid Phone Number',
+          style: montserrat(Colors.white, h3, FontWeight.normal),
+        ),
+        backgroundColor: Colors.red,
+        duration: Duration(milliseconds: 700),
+      );
       ScaffoldMessenger.of(context).showSnackBar(snackBar);
-    }
-    else{
+    } else {
       await FirebaseAuth.instance.verifyPhoneNumber(
-        phoneNumber: "+91"+ phoneNumber.toString(),
-        verificationCompleted: (PhoneAuthCredential credential) async {
-        },
+        phoneNumber: "+91" + phoneNumber.toString(),
+        verificationCompleted: (PhoneAuthCredential credential) async {},
         verificationFailed: (FirebaseAuthException e) {
           final snackBar = SnackBar(content: Text('Something went wrong'));
           ScaffoldMessenger.of(context).showSnackBar(snackBar);
         },
-        codeSent: (String verificationId, int? resendToken)async {
+        codeSent: (String verificationId, int? resendToken) async {
           final snackBar = SnackBar(content: Text('Code sent!'));
           ScaffoldMessenger.of(context).showSnackBar(snackBar);
           String smsCode = 'xxxx';
-          PhoneAuthCredential credential = PhoneAuthProvider.credential(verificationId: verificationId, smsCode: smsCode);
+          PhoneAuthCredential credential = PhoneAuthProvider.credential(
+              verificationId: verificationId, smsCode: smsCode);
 
           // Sign the user in (or link) with the credential
-          await  FirebaseAuth.instance.signInWithCredential(credential);
+          await FirebaseAuth.instance.signInWithCredential(credential);
         },
         codeAutoRetrievalTimeout: (String verificationId) {},
       );
     }
-
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -58,23 +63,22 @@ class _LoginPageState extends State<LoginPage> {
               Logo(),
               Container(
                 alignment: Alignment.centerLeft,
-                margin: EdgeInsets.only(left: 20,right: 20),
+                margin: EdgeInsets.only(left: 20, right: 20),
                 child: Text(
                   "Phone number",
-                  style: poppins(black.withOpacity(0.75),h3,FontWeight.normal),
+                  style:
+                      poppins(black.withOpacity(0.75), h3, FontWeight.normal),
                 ),
               ),
               Container(
                 height: 50,
                 padding: EdgeInsets.all(5),
                 width: double.infinity,
-                margin: EdgeInsets.only(left: 20,right: 20),
+                margin: EdgeInsets.only(left: 20, right: 20),
                 decoration: BoxDecoration(
                   color: textBoxColor,
-                  borderRadius:BorderRadius.circular(5),
-                  border: Border.all(
-                    color: Colors.grey.shade400
-                  ),
+                  borderRadius: BorderRadius.circular(5),
+                  border: Border.all(color: Colors.grey.shade400),
                   boxShadow: [
                     BoxShadow(
                       color: Colors.grey.shade300,
@@ -89,31 +93,29 @@ class _LoginPageState extends State<LoginPage> {
                 ),
                 child: Center(
                   child: TextField(
-                      onChanged: (value){
+                      onChanged: (value) {
                         phoneNumber = value;
                       },
                       decoration: InputDecoration.collapsed(hintText: ""),
                       keyboardType: TextInputType.number,
-                      style: poppins(black,h2,FontWeight.w600)
-                  ),
+                      style: poppins(black, h2, FontWeight.w600)),
                 ),
               ),
               InkWell(
-                onTap: (){
+                onTap: () {
                   phoneNumberLogin();
                 },
                 child: Container(
-                  margin: EdgeInsets.only(left:15,right: 15,top: 30,bottom: 30),
+                  margin:
+                      EdgeInsets.only(left: 15, right: 15, top: 30, bottom: 30),
                   height: 75,
                   width: 250,
                   decoration: BoxDecoration(
-                    color: boxRed,
-                    borderRadius: BorderRadius.circular(10)
-                  ),
+                      color: boxRed, borderRadius: BorderRadius.circular(10)),
                   child: Center(
                     child: Text(
                       "Get OTP",
-                      style: montserrat(boxRedText,h2,FontWeight.w700) ,
+                      style: montserrat(boxRedText, h2, FontWeight.w700),
                     ),
                   ),
                 ),
@@ -126,11 +128,8 @@ class _LoginPageState extends State<LoginPage> {
               Container(
                 height: 75,
                 decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: Image.asset('assets/a.png').image,
-                    scale: 0.5
-                  )
-                ),
+                    image: DecorationImage(
+                        image: Image.asset('assets/a.png').image, scale: 0.5)),
               ),
             ],
           )
@@ -151,18 +150,17 @@ class Logo extends StatelessWidget {
         child: RichText(
           text: TextSpan(
               text: 'GEU\n',
-              style: montserrat(black.withOpacity(0.5),h3,FontWeight.w600),
+              style: montserrat(black.withOpacity(0.5), h3, FontWeight.w600),
               children: <TextSpan>[
-                TextSpan(text: 'BUS\n',
-                  style: montserrat(logoRed,h1+40,FontWeight.bold),
+                TextSpan(
+                  text: 'BUS\n',
+                  style: montserrat(logoRed, h1 + 40, FontWeight.bold),
                 ),
-                TextSpan(text: 'FINDER\n',
-                  style: montserrat(black,h1+50,FontWeight.bold),
+                TextSpan(
+                  text: 'FINDER\n',
+                  style: montserrat(black, h1 + 50, FontWeight.bold),
                 )
-              ]
-          ),
-        )
-    );
+              ]),
+        ));
   }
 }
-
